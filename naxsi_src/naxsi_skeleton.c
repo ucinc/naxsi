85810db8b97486e1293730a9831b00642c1aaba9
orixxx@gmail.com <orixxx@gmail.com@38cfc0d1-1855-38ab-c664-02d32d4e92d7>
2011-07-28 10:18:40 +0000

    added licence headers

--- naxsi_src/naxsi_skeleton.c
+++ naxsi_src/naxsi_skeleton.c
@@ -1,4 +1,22 @@
 /*
+ * NAXSI, a web application firewall for NGINX
+ * Copyright (C) 2011, Thibault 'bui' Koechlin
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
 ** This files contains skeleton functions, 
 ** such as registred handlers. Readers already 
 ** aware of nginx's modules can skip most of this.
