b198649a617dbebff1404b42efd53765f97a4e1f
orixxx@gmail.com <orixxx@gmail.com@38cfc0d1-1855-38ab-c664-02d32d4e92d7>
2012-09-18 14:42:51 +0000

    added command 'imports' to allow manual log import.

--- branches/django/naxsi_ui/nx_extract/management/commands/imports.py
+++ branches/django/naxsi_ui/nx_extract/management/commands/imports.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 #Standalone command to allow offline logfile injection
 from nx_extract.tailer import Tailer
 from nx_extract.models import nx_fmt, Zone, InputType
-
+import pprint
 
 def dummy_callback(mdict, output, mworld):
 #    return
@@ -40,17 +40,17 @@ def dummy_callback(mdict, output, mworld):
 
 
 class Command(BaseCommand):
-    args = '/path/to/log_file [start-end] [-end]\n'
+    args = '/path/to/log_file [start-end] [-end] [start-]\n'
     args += 'Periods are in the format: Y/M/D H:M:S\n'
     args += 'ie. /tmp/log1 "2012/1/13 00:00:00-2012/2/31 00:00:00"\n'
     args += 'or: /tmp/log1 "2012/1/13 00:00:00-" (everything AFTER 2012/1/13)\n'
     args += 'or: /tmp/log1 "-2012/1/13 00:00:00" (everything BEFORE 2012/1/13)\n'
-    help = 'Import specified log files\n'
+    mhelp = 'Import specified log files\n'
 
     def handle(self, *args, **options):
         if len(args) < 1:
-            self.stdout.write(help)
-            self.stdout.write(args)
+            self.stdout.write(self.mhelp)
+            self.stdout.write(self.args)
             return
         self.stdout.write('Preparing to import ['+args[0]+']\n')
         self.stdout.write('Currently, there is '+str(nx_fmt.objects.count())+" exception objects\n")
@@ -58,8 +58,29 @@ class Command(BaseCommand):
         if log.open_log() is False:
             self.stdout.write("Unable to open "+log.filename+"\n")
             return
+        periods=[]
+        cpt=1
+        while cpt < len(args):
+            print "PERIOD ? "+args[cpt]
+            prds = args[cpt].split("-")
+            if len(prds) != 2:
+                self.stdout.write("Period(s) must be interval, splitted by '-'")
+                return
+            sub = []
+            sub.append(prds[0])
+            sub.append(prds[1])
+            periods.append(sub)
+            cpt += 1
         self.stdout.write("Starting full import ...\n")
-        ret = log.backlog(output=None, callback=dummy_callback)
+        if len(periods) > 0:
+            self.stdout.write("Limiting import to periods:\n")
+            pprint.pprint(periods)
+            ret = log.backlog(output=None, callback=dummy_callback, backlog=periods)
+        else:
+            self.stdout.write("Full import, no period specified.\n")
+            ret = log.backlog(output=None, callback=dummy_callback)
+#        return
+
         self.stdout.write("Imported "+str(len(ret))+" items.\n")
         while len(ret):
             sub = ret[:50]
